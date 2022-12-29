4b19f9139464b3dd39be5a8b0eccc19587677b16
André Santanchè <santanche@gmail.com>
2022-12-29 09:31:14 -0300

    refactor(homepage): transferring old version to a folder (step 1)

--- js/standard.js
+++
@@ -1,9 +0,0 @@
-function switchDisplayDiv(id) {
-    var divElem = document.getElementById(id);
-    if (divElem != null && divElem.style != null) {
-        if (divElem.style.display === null || divElem.style.display === "")
-            divElem.style.display = "none";
-        else
-            divElem.style.display = "";
-    }
-}
